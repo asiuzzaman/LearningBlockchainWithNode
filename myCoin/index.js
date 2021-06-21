6f80a0668249ecb89e05e62003d3a239cfea3d3c
MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>
2021-06-21 18:15:31 +0600

    [Blockchain] Creating a new block
    Signed-off-by: MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>

--- myCoin/index.js
+++ myCoin/index.js
@@ -1,1 +1,16 @@
-console.log("Hello blockchain");
+const sha256 = require('crypto-js/sha256');
+class Block{
+    constructor(timestamp,data,previousHash){
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash(){
+        return sha256(this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+    }
+
+}
+const block = new Block("2021-06-21", {amount: 5}, "ABCE");
+
+console.log(block);

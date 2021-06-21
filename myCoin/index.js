296bee9b6f54140d6bed577be9d3714c81b5c9bc
MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>
2021-06-21 18:53:52 +0600

    [Blockchain] Creating a very basic blockchain
    Signed-off-by: MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>

--- myCoin/index.js
+++ myCoin/index.js
@@ -1,6 +1,6 @@
 const sha256 = require('crypto-js/sha256');
 class Block{
-    constructor(timestamp,data,previousHash){
+    constructor(timestamp,data,previousHash = ""){
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
@@ -14,16 +14,27 @@ class Block{
 
 class Blockchain{
     constructor(){
-        this.chain = [];
+        this.chain = [this.generateGenesisBlock()];
+       // this.chain.push();
     }
 
+    generateGenesisBlock(){
+        return new Block("2021-06-21","GENESIS","0000");
+    }
+    getLatestBlock(){
+        let lengthOfChain = this.chain.length;
+        // Return Last element of chain
+        return this.chain[lengthOfChain-1];
+    }
     addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
 }
 
 const myCoin = new Blockchain();
-const block = new Block("2021-06-21", {amount: 5}, "ABCE");
+const block = new Block("2021-06-22", {amount: 5});
 
 
 

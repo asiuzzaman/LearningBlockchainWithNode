7b7f5cbb0b008f72a0b46119be66288b23b0cd9c
MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>
2021-06-22 13:06:06 +0600

    [Blockchain] Modify Transaction Class
    Signed-off-by: MOHAMMAD ASIUZZAMAN <zaman1304006@gmail.com>

--- myCoin/index.js
+++ myCoin/index.js
@@ -1,4 +1,6 @@
 const sha256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
 class Block{
     constructor(timestamp,transaction,previousHash = ""){
         this.timestamp = timestamp;
@@ -34,6 +36,34 @@ class Transaction{
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash(){
+        return sha256(this.fromAddress + this.toAddress + this.amount);
+    }
+
+    signTransaction(key){
+        if(key.getPublic("hex") !== this.fromAddress) { // problem 
+            throw new Error ( "You don't have access");
+        }
+
+        const hashTx = this.calculateHash();
+
+        const signature = key.sign(hashTx,"base64");
+        this.signature = signature.toDER();
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true;
+
+        if(!this.signature || this.signature.length === 0) {
+            throw new Error("No signature found");
+        }
+        
+       const key = ec.keyFromPublic(this.fromAddress,"hex");
+
+      return key.verify(this.calculateHash,this.signature);
+    
     }
 }
 
